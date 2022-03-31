0aae02f56e263b08dfa221df8d6e6d5ed3ad00a5
sh92.shin <simsonhyun@gmail.com>
2022-03-31 15:32:31 +0900

    header 파일 추가.

--- ClarifyPJT/ClarifyPJT/RunProgram.h
+++ ClarifyPJT/ClarifyPJT/RunProgram.h
@@ -1,18 +1,6 @@
 #pragma once
-#include "CommandParser.h"
 
 class RunProgram
 {
-public:
-	const void run(const string& cmdString)
-	{
-		InputParameter inputParameter = commandParser.ConvertParameter(cmdString);
 
-
-	}
-private:
-
-public:
-private:
-	CommandParser commandParser;
 };
