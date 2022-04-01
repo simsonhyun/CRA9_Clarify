0185a1f04aa22a4d82abae3ab1b5e22747bf916e
DongyeonKwak <102642731+DongyeonKwak@users.noreply.github.com>
2022-04-01 10:19:01 +0900

    [Feature][Search(non-option) 구현]
    non-option search(employeeNum/CL/CERTI) 구현

---
+++ ClarifyPJT/ClarifyPJT/DataManager.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include "Employee.h"
+using namespace std;
+
+class DataManager
+{
+public:
+	DataManager()
+	{
+		_employees.reserve(MAX_EMPLOYEE_COUNT);
+	}
+
+	const vector<Employee>& getData(void)
+	{
+		return _employees;
+	}
+
+	void setData(const Employee& employee) {
+		_employees.push_back(employee);
+	}
+
+private:
+
+
+
+public:
+	static const int MAX_EMPLOYEE_COUNT = 1500000;
+
+private:
+	vector<Employee> _employees;
+
+
+};
