64e1e0f80af14edc4de43bfac2b9a33f5353ed4b
sh92.shin <simsonhyun@gmail.com>
2022-04-01 09:22:14 +0900

    [Fix][Build Error Fix]
    rebase 후 build Error Fix

--- ClarifyPJT/TestProject/test_Add.cpp
+++ ClarifyPJT/TestProject/test_Add.cpp
@@ -12,17 +12,16 @@ TEST(TEST_ADD_MODULE, ADD_TEST) {
 	birthday._year = 1990;
 	birthday._month = 01;
 	birthday._day = 23;
-	Employee employee(12345678, Name("GILDONG", "HONG"), CareerLevel::CareerLevel_2, phonenum, birthday, Certi::Certi_PRO);
-	InputParameter inputParameter = { Command::Command_None, Option1::Option1_None, Option2::Option2_None, Column::Column_None, Column::Column_None, employee };
-
+	Employee employee(12345678, "홍길동", CareerLevel_2, phonenum, birthday, Certi_PRO);
+	InputParameter inputParameter = { Command_None, Option1_null, Option2_null, Column_null, employee };
+	
 	add.Command(inputParameter);
 
 	ASSERT_TRUE(dataManager.getSize() != 0);
 	vector<Employee> employees = dataManager.getData();
 	Employee* result = employees.data();
 	EXPECT_EQ(result->GetEmployeeNum(), employee.GetEmployeeNum());
-	EXPECT_EQ(result->GetName()._firstName, employee.GetName()._firstName);
-	EXPECT_EQ(result->GetName()._lastName, employee.GetName()._lastName);
+	EXPECT_EQ(result->GetName(), employee.GetName());
 	EXPECT_EQ(result->GetCl(), employee.GetCl());
 	EXPECT_EQ(result->GetPhoneNum()._middleNum, employee.GetPhoneNum()._middleNum);
 	EXPECT_EQ(result->GetPhoneNum()._lastNum, employee.GetPhoneNum()._lastNum);
@@ -48,22 +47,21 @@ TEST(TEST_ADD_MODULE, ADD_MAX_SIZE_TEST) {
 	int employeeNum = 90000000;
 	for (int i = 0; i < 100000; i++, employeeNum++)
 	{
-		Employee inputemployee(employeeNum, Name("GILDONG", "HONG"), CareerLevel::CareerLevel_2, phonenum, birthday, Certi::Certi_PRO);
-		InputParameter inputParameter = { Command::Command_None, Option1::Option1_None, Option2::Option2_None, Column::Column_None, Column::Column_None, inputemployee };
+		Employee inputemployee(employeeNum, "홍길동", CareerLevel_2, phonenum, birthday, Certi_PRO);
+		InputParameter inputParameter = { Command_None, Option1_null, Option2_null, Column_null, inputemployee };
 		add.Command(inputParameter);
 	}
 
 	ASSERT_TRUE(dataManager.getSize() == 100000);
-
+	
 	employeeNum = 90000000;
 	vector<Employee> employees = dataManager.getData(); //employeenum 순서대로 sorting되든, 입력대로 add되든 test가능
-	for (auto employee : employees)
+	for(auto employee: employees)
 	{
-		Employee inputEmployee(employeeNum++, Name("GILDONG", "HONG"), CareerLevel::CareerLevel_2, phonenum, birthday, Certi::Certi_PRO);
+		Employee inputEmployee(employeeNum++, "홍길동", CareerLevel_2, phonenum, birthday, Certi_PRO);
 		Employee* result = &employee;
 		EXPECT_EQ(result->GetEmployeeNum(), inputEmployee.GetEmployeeNum());
-		EXPECT_EQ(result->GetName()._firstName, inputEmployee.GetName()._firstName);
-		EXPECT_EQ(result->GetName()._lastName, inputEmployee.GetName()._lastName);
+		EXPECT_EQ(result->GetName(), inputEmployee.GetName());
 		EXPECT_EQ(result->GetCl(), inputEmployee.GetCl());
 		EXPECT_EQ(result->GetPhoneNum()._middleNum, inputEmployee.GetPhoneNum()._middleNum);
 		EXPECT_EQ(result->GetPhoneNum()._lastNum, inputEmployee.GetPhoneNum()._lastNum);
