353c393bd99dd4346514745fe48318110c652e58
sh92.shin <simsonhyun@gmail.com>
2022-04-04 10:56:31 +0900

    fileManager fix

--- ClarifyPJT/ClarifyPJT/FileManager.h
+++ ClarifyPJT/ClarifyPJT/FileManager.h
@@ -13,7 +13,15 @@ public:
 
 	const void OutputOpen(const string& outputText)
 	{
-		outputFile.open(outputText);
+		if (IsFileExists(outputText) == true)
+		{
+			outputFile.open(outputText);
+		}
+		else
+		{
+			ofstream createfile (outputText);
+			outputFile.open(outputText);
+		}
 	}
 
 	const void InputClose(void)
@@ -48,7 +56,34 @@ public:
 		outputFile << inputString;
 	}
 
+
+	bool compareFiles(const std::string& p1, const std::string& p2) 
+	{
+		std::ifstream f1(p1, std::ifstream::binary | std::ifstream::ate);
+		std::ifstream f2(p2, std::ifstream::binary | std::ifstream::ate);
+
+		if (f1.fail() || f2.fail()) {
+			return false; //file problem
+		}
+
+		if (f1.tellg() != f2.tellg()) {
+			return false; //size mismatch
+		}
+
+		//seek back to beginning and use std::equal to compare contents
+		f1.seekg(0, std::ifstream::beg);
+		f2.seekg(0, std::ifstream::beg);
+		return std::equal(std::istreambuf_iterator<char>(f1.rdbuf()),
+			std::istreambuf_iterator<char>(),
+			std::istreambuf_iterator<char>(f2.rdbuf()));
+	}
+
 private:
+	bool IsFileExists(const string& fileName) 
+	{
+		ifstream f(fileName.c_str());
+		return f.good();
+	}
 
 
 public:
