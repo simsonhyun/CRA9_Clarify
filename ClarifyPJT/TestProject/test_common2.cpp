5ba1b7f54123224c53db189f95e9f55d38044a53
sh92.shin <simsonhyun@gmail.com>
2022-04-04 15:59:53 +0900

    del test 추가

--- ClarifyPJT/TestProject/test_common2.cpp
+++ ClarifyPJT/TestProject/test_common2.cpp
@@ -4,7 +4,15 @@
 TEST(RunTest2, CheckRun1)
 {
 	RunProgram runProgram;
-	runProgram.run("TestCase/input_20_20.txt", "CommonTestFiles/output_20_20_test.txt");
+	runProgram.run("TestCase/input_20_20.txt", "CommonTestFiles/output_20_20_Result.txt");
 
 	EXPECT_TRUE(runProgram.compareFiles("TestCase/output_20_20.txt", "CommonTestFiles/output_20_20_test.txt"));
+}
+
+TEST(RunTest2, CheckRun2)
+{
+	RunProgram runProgram;
+	runProgram.run("CommonTestFiles/inputDelTest.txt", "CommonTestFiles/outputDelTestResult.txt");
+
+	EXPECT_TRUE(runProgram.compareFiles("CommonTestFiles/outputDelTestCorrect.txt", "CommonTestFiles/outputDelTestResult.txt"));
 }
