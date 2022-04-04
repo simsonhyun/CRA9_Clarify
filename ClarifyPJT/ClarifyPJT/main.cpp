6831b758371c137c554a1301a7ddabc588e9a22d
sh92.shin <simsonhyun@gmail.com>
2022-04-04 10:56:31 +0900

    fileManager fix

--- ClarifyPJT/ClarifyPJT/main.cpp
+++ ClarifyPJT/ClarifyPJT/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 	RunProgram* runProgram = new RunProgram();
 
 	runProgram->init();
-	runProgram->run("ADD, , , ,17111236,VSID TVO,CL1,010-3669-1077,20120718,PRO");
+	runProgram->_run("ADD, , , ,17111236,VSID TVO,CL1,010-3669-1077,20120718,PRO");
 
 	return 0;
 }
