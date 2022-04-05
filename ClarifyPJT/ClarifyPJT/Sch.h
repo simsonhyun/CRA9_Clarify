090022f601048031ed9f7611acf11d2a3b66e40a
DongYeon <qpoas3@naver.com>
2022-04-05 12:33:21 +0900

    [Refactor/Test][Search ouput return 변경 및 print option] - print option이 있을때만 vector push - Test code 변경 - EmployeeNum search시에 sort 되어 있으므로 input보다 높은 사번일 경우   return

--- ClarifyPJT/ClarifyPJT/Sch.h
+++ ClarifyPJT/ClarifyPJT/Sch.h
@@ -3,67 +3,149 @@
 
 class AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const = 0;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const = 0;
+
+	static const int MAX_EMPLOYEE_COUNT = 1500000;
+	static const int MAX_PRINT_SIZE = 5;
 };
 
 class EmployeeNumSearchPolicy : public AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const override {
-		if (option == Option2::Option2_None && input.GetEmployeeNum() != data.GetEmployeeNum()) return false;
-		return true;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const override {
+		OutputParameter result;
+		result.resultCount = 0;
+		for (const Employee& datum : data) {
+			if (input.option2 == Option2::Option2_None && input.inputEmployee.GetEmployeeNum() == datum.GetEmployeeNum()) {
+				result.resultVector.push_back(datum);
+				result.resultCount++;
+				return result;
+			}
+		}
+		return result;
 	}
 };
 
 class NameSearchPolicy : public AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const override {
-		if (option == Option2::Option2_Name_f && input.GetName()._firstName != data.GetName()._firstName) return false;
-		if (option == Option2::Option2_Name_l && input.GetName()._lastName != data.GetName()._lastName) return false;
-		if (option == Option2::Option2_None && input.GetName() != data.GetName()) return false;
-		return true;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const override {
+		OutputParameter result;
+		if (input.option1 == Option1::Option1_p) result.resultVector.reserve(MAX_EMPLOYEE_COUNT);
+		else result.resultCount = 0;
+
+		for (const Employee& datum : data) {
+			if (input.option2 == Option2::Option2_Name_f && input.inputEmployee.GetName()._firstName == datum.GetName()._firstName) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_Name_l && input.inputEmployee.GetName()._lastName == datum.GetName()._lastName) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_None && input.inputEmployee.GetName() == datum.GetName()) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option1 == Option1::Option1_p && result.resultVector.size() >= MAX_PRINT_SIZE) return result;
+		}
+		return result;
 	}
 };
 
 class CareerLevelSearchPolicy : public AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const override {
-		if (option == Option2::Option2_None && input.GetCl() != data.GetCl()) return false;
-		return true;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const override {
+		OutputParameter result;
+		if (input.option1 == Option1::Option1_p) result.resultVector.reserve(MAX_EMPLOYEE_COUNT);
+		else result.resultCount = 0;
+
+		for (const Employee& datum : data) {
+			if (input.option2 == Option2::Option2_None && input.inputEmployee.GetCl() == datum.GetCl()) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option1 == Option1::Option1_p && result.resultVector.size() >= MAX_PRINT_SIZE) return result;
+		}
+		return result;
 	}
 };
 
 class PhoneNumSearchPolicy : public AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const override {
-		if (option == Option2::Option2_PhoneNum_l && input.GetPhoneNum()._lastNum != data.GetPhoneNum()._lastNum) return false;
-		if (option == Option2::Option2_PhoneNum_m && input.GetPhoneNum()._middleNum != data.GetPhoneNum()._middleNum) return false;
-		if (option == Option2::Option2_None && input.GetPhoneNum() != data.GetPhoneNum()) return false;
-		return true;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const override {
+		OutputParameter result;
+		if (input.option1 == Option1::Option1_p) result.resultVector.reserve(MAX_EMPLOYEE_COUNT);
+		else result.resultCount = 0;
+
+		for (const Employee& datum : data) {
+			if (input.option2 == Option2::Option2_PhoneNum_l && input.inputEmployee.GetPhoneNum()._lastNum == datum.GetPhoneNum()._lastNum) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_PhoneNum_m && input.inputEmployee.GetPhoneNum()._middleNum == datum.GetPhoneNum()._middleNum) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_None && input.inputEmployee.GetPhoneNum() == datum.GetPhoneNum()) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option1 == Option1::Option1_p && result.resultVector.size() >= MAX_PRINT_SIZE) return result;
+		}
+		return result;
 	}
 };
 
 class BirthDaySearchPolicy : public AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const override {
-		if (option == Option2::Option2_BirthDay_y && input.GetBirthDay()._year != data.GetBirthDay()._year) return false;
-		if (option == Option2::Option2_BirthDay_m && input.GetBirthDay()._month != data.GetBirthDay()._month) return false;
-		if (option == Option2::Option2_BirthDay_d && input.GetBirthDay()._day != data.GetBirthDay()._day) return false;
-		if (option == Option2::Option2_None && input.GetBirthDay() != data.GetBirthDay()) return false;
-		return true;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const override {
+		OutputParameter result;
+		if (input.option1 == Option1::Option1_p) result.resultVector.reserve(MAX_EMPLOYEE_COUNT);
+		else result.resultCount = 0;
+
+		for (const Employee& datum : data) {
+			if (input.option2 == Option2::Option2_BirthDay_y && input.inputEmployee.GetBirthDay()._year == datum.GetBirthDay()._year) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_BirthDay_m && input.inputEmployee.GetBirthDay()._month == datum.GetBirthDay()._month) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_BirthDay_d && input.inputEmployee.GetBirthDay()._day == datum.GetBirthDay()._day) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option2 == Option2::Option2_None && input.inputEmployee.GetBirthDay() == datum.GetBirthDay()) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option1 == Option1::Option1_p && result.resultVector.size() >= MAX_PRINT_SIZE) return result;
+		}
+		return result;
 	}
 };
 
 class CertiSearchPolicy : public AbstractSearchPolicy {
 public:
-	virtual bool searchByPolicy(Option2 option, const Employee& input, const Employee& data) const override {
-		if (option == Option2::Option2_None && input.GetCerti() != data.GetCerti()) return false;
-		return true;
+	virtual OutputParameter searchByPolicy(InputParameter input, const vector<Employee>& data) const override {
+		OutputParameter result;
+		if (input.option1 == Option1::Option1_p) result.resultVector.reserve(MAX_EMPLOYEE_COUNT);
+		else result.resultCount = 0;
+
+		for (const Employee& datum : data) {
+			if (input.option2 == Option2::Option2_None && input.inputEmployee.GetCerti() == datum.GetCerti()) {
+				if (input.option1 == Option1::Option1_p) result.resultVector.push_back(datum);
+				else result.resultCount++;
+			}
+			if (input.option1 == Option1::Option1_p && result.resultVector.size() >= MAX_PRINT_SIZE) return result;
+		}
+		return result;
 	}
 };
 
 class Search : public CommandManager {
 public:
-	Search(DataManager& dataManager) : CommandManager(CommandType::Command_Sch, dataManager) {
+	Search(DataManager& dataManager) : CommandManager(CommandType::Command_Sch, dataManager), _dataManager(dataManager) {
 		searchPolicy[static_cast<int>(Column::Column_employeeNum)] = new EmployeeNumSearchPolicy();
 		searchPolicy[static_cast<int>(Column::Column_Name)] = new NameSearchPolicy();
 		searchPolicy[static_cast<int>(Column::Column_CareerLevel)] = new CareerLevelSearchPolicy();
@@ -73,18 +155,12 @@ public:
 	}
 
 	OutputParameter Command(InputParameter input) override {
-		vector<Employee> result;
-		for (const Employee& datum : getDataManager()->getData()) {
-			if (searchPolicy[static_cast<int>(input.column)]->searchByPolicy(input.option2, input.inputEmployee, datum))
-				result.push_back(datum);
-			if (input.option1 == Option1::Option1_p && result.size() >= 5) return OutputParameter(); // return result; 
-		}
-		// return result;
-		return OutputParameter();
+		return searchPolicy[static_cast<int>(input.column)]->searchByPolicy(input, _dataManager.getData());
 	}
 
 public:
 	static const int MAX_COLUMN = 6;
 private:
 	AbstractSearchPolicy* searchPolicy[MAX_COLUMN];
+	DataManager& _dataManager;
 };
--- ClarifyPJT/TestProject/test_Sch.cpp
+++ ClarifyPJT/TestProject/test_Sch.cpp
@@ -5,8 +5,8 @@ class SearchTest : public ::testing::Test {
 protected:
 	void SetUp() override {
 		Employee employee1 = { 12345678, {"È«", "±æµ¿"}, CareerLevel::CareerLevel_1,  {1234, 5678}, {1990, 01, 01}, Certi::Certi_ADV };
-		Employee employee2 = { 87654321, {"±è", "Ã¶¼ö"}, CareerLevel::CareerLevel_2,  {1234, 8765}, {1990, 02, 01}, Certi::Certi_PRO };
-		Employee employee3 = { 12345678, {"ÀÌ", "¿µÈñ"}, CareerLevel::CareerLevel_3,  {9876, 5678}, {1990, 01, 02}, Certi::Certi_EX };
+		Employee employee2 = { 87654322, {"±è", "Ã¶¼ö"}, CareerLevel::CareerLevel_2,  {1234, 8765}, {1990, 02, 01}, Certi::Certi_PRO };
+		Employee employee3 = { 12345679, {"ÀÌ", "¿µÈñ"}, CareerLevel::CareerLevel_3,  {9876, 5678}, {1990, 01, 02}, Certi::Certi_EX };
 		Employee employee4 = { 87654321, {"°í", "±æµ¿"}, CareerLevel::CareerLevel_4,  {1354, 9516}, {1990, 03, 02}, Certi::Certi_ADV };
 		Employee employee5 = { 11111111, {"È«", "±æµ¿"}, CareerLevel::CareerLevel_1,  {9513, 7412}, {1990, 04, 03}, Certi::Certi_PRO };
 		dataManager->setData(employee1);
@@ -27,124 +27,157 @@ TEST_F(SearchTest, SearchByEmployeeNumTest) {
 	Employee searchEmployee = { 12345678, {"±è", "Ã¶¼ö"}, CareerLevel::CareerLevel_2,  {1234, 8765}, {1990, 02, 01}, Certi::Certi_PRO };
 	InputParameter input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_None, Column::Column_employeeNum, Column::Column_None, searchEmployee };
 
-	vector<Employee> result = searchTest->Command(input);
+	OutputParameter out = searchTest->Command(input);
+	vector<Employee> result = out.resultVector;
+	int resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetEmployeeNum(), searchEmployee.GetEmployeeNum());
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(1, resultCnt);
 };
 
 TEST_F(SearchTest, SearchByCareerLevelTest) {
 	Employee searchEmployee = { 11111111, {"È«", "±æµ¿"}, CareerLevel::CareerLevel_1,  {9513, 7412}, {1990, 04, 03}, Certi::Certi_PRO };
 	InputParameter input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_None, Column::Column_CareerLevel, Column::Column_None, searchEmployee };
 
-	vector<Employee> result = searchTest->Command(input);
+	OutputParameter out = searchTest->Command(input);
+	vector<Employee> result = out.resultVector;
+	int resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetCl(), searchEmployee.GetCl());
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 };
 
 TEST_F(SearchTest, SearchByCertiTest) {
 	Employee searchEmployee = { 87654321, {"°í", "±æµ¿"}, CareerLevel::CareerLevel_4,  {1354, 9516}, {1990, 03, 02}, Certi::Certi_ADV };
 	InputParameter input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_None, Column::Column_Certi, Column::Column_None, searchEmployee };
 
-	vector<Employee> result = searchTest->Command(input);
+	OutputParameter out = searchTest->Command(input);
+	vector<Employee> result = out.resultVector;
+	int resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetCerti(), searchEmployee.GetCerti());
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 };
 
 TEST_F(SearchTest, SearchByNameTest) {
 	Employee searchEmployee = { 87654321, {"È«", "±æµ¿"}, CareerLevel::CareerLevel_4,  {1354, 9516}, {1990, 03, 02}, Certi::Certi_ADV };
 	InputParameter input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_None, Column::Column_Name, Column::Column_None, searchEmployee };
 
-	vector<Employee> result = searchTest->Command(input);
+	OutputParameter out = searchTest->Command(input);
+	vector<Employee> result = out.resultVector;
+	int resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetName()._firstName, searchEmployee.GetName()._firstName);
 		EXPECT_EQ(ep.GetName()._lastName, searchEmployee.GetName()._lastName);
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_Name_f, Column::Column_Name, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetName()._firstName, searchEmployee.GetName()._firstName);
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_Name_l, Column::Column_Name, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetName()._lastName, searchEmployee.GetName()._lastName);
 	}
-	EXPECT_EQ(3, result.size());
+	EXPECT_EQ(3, resultCnt);
 };
 
 TEST_F(SearchTest, SearchByPhoneNumTest) {
 	Employee searchEmployee = { 12345678, {"È«", "±æµ¿"}, CareerLevel::CareerLevel_1,  {1234, 5678}, {1990, 01, 01}, Certi::Certi_ADV };
 	InputParameter input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_None, Column::Column_PhoneNum, Column::Column_None, searchEmployee };
 
-	vector<Employee> result = searchTest->Command(input);
+	OutputParameter out = searchTest->Command(input);
+	vector<Employee> result = out.resultVector;
+	int resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetPhoneNum()._middleNum, searchEmployee.GetPhoneNum()._middleNum);
 		EXPECT_EQ(ep.GetPhoneNum()._lastNum, searchEmployee.GetPhoneNum()._lastNum);
 	}
-	EXPECT_EQ(1, result.size());
+	EXPECT_EQ(1, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_PhoneNum_m, Column::Column_PhoneNum, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetPhoneNum()._middleNum, searchEmployee.GetPhoneNum()._middleNum);
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_PhoneNum_l, Column::Column_PhoneNum, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetPhoneNum()._lastNum, searchEmployee.GetPhoneNum()._lastNum);
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 };
 
 TEST_F(SearchTest, SearchByBirthDayTest) {
 	Employee searchEmployee = { 12345678, {"È«", "±æµ¿"}, CareerLevel::CareerLevel_1,  {1234, 5678}, {1990, 01, 01}, Certi::Certi_ADV };
 	InputParameter input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_None, Column::Column_BirthDay, Column::Column_None, searchEmployee };
 
-	vector<Employee> result = searchTest->Command(input);
+	OutputParameter out = searchTest->Command(input);
+	vector<Employee> result = out.resultVector;
+	int resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetBirthDay()._year, searchEmployee.GetBirthDay()._year);
 		EXPECT_EQ(ep.GetBirthDay()._month, searchEmployee.GetBirthDay()._month);
 		EXPECT_EQ(ep.GetBirthDay()._day, searchEmployee.GetBirthDay()._day);
 	}
-	EXPECT_EQ(1, result.size());
+	EXPECT_EQ(1, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_BirthDay_y, Column::Column_BirthDay, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetBirthDay()._year, searchEmployee.GetBirthDay()._year);
 	}
-	EXPECT_EQ(5, result.size());
+	EXPECT_EQ(5, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_BirthDay_m, Column::Column_BirthDay, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetBirthDay()._month, searchEmployee.GetBirthDay()._month);
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 
 	input = { CommandType::Command_Sch, Option1::Option1_None, Option2::Option2_BirthDay_d, Column::Column_BirthDay, Column::Column_None, searchEmployee };
+	out = searchTest->Command(input);
 	result.clear();
-	result = searchTest->Command(input);
+	result = out.resultVector;
+	result = searchTest->Command(input).resultVector;
+	resultCnt = out.resultCount;
 	for (const Employee& ep : result) {
 		EXPECT_EQ(ep.GetBirthDay()._day, searchEmployee.GetBirthDay()._day);
 	}
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, resultCnt);
 };
