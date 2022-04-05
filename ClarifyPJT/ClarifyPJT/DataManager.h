68040408f16190b362c718a4ed3c71720d0d8a76
Juyeon Park <kitty4701@gmail.com>
2022-04-05 12:39:03 +0900

    [feature][sort] 기존 sort 하던 코드 제거

--- ClarifyPJT/ClarifyPJT/DataManager.h
+++ ClarifyPJT/ClarifyPJT/DataManager.h
@@ -11,16 +11,10 @@ public:
 	DataManager()
 	{
 		_employees.reserve(MAX_EMPLOYEE_COUNT);
-		bIsSorted = false;
 	}
 
 	vector<Employee>& getData(void)
 	{
-		if (!bIsSorted)
-		{
-			sort(_employees.begin(), _employees.end(), DataManager::compare);
-			bIsSorted = true;
-		}
 		return _employees;
 	}
 
@@ -31,17 +25,7 @@ public:
 
 	void setData(const Employee& employee)
 	{
-		bIsSorted = false;
 		_employees.push_back(employee);
-	}
-
-	static bool compare(Employee a, Employee b)
-	{
-		if (((a.GetEmployeeNum() <= Employee::MAX_EMPLOYEE_NUM) && (b.GetEmployeeNum() <= Employee::MAX_EMPLOYEE_NUM))
-				|| ((a.GetEmployeeNum() >= Employee::MIN_EMPLOYEE_NUM) && (b.GetEmployeeNum() >= Employee::MIN_EMPLOYEE_NUM)))
-			return a.GetEmployeeNum() < b.GetEmployeeNum();
-
-		return a.GetEmployeeNum() > b.GetEmployeeNum();
 	}
 
 private:
@@ -51,5 +35,4 @@ public:
 
 private:
 	vector<Employee> _employees;
-	bool bIsSorted;
 };
--- ClarifyPJT/TestProject/test_Add.cpp
+++ ClarifyPJT/TestProject/test_Add.cpp
@@ -74,52 +74,6 @@ TEST(TEST_ADD_MODULE, ADD_MAX_SIZE_TEST) {
 	}
 }
 
-TEST(TEST_ADD_MODULE, ADD_SORT_TEST) {
-	DataManager dataManager = DataManager();
-	Add add = Add(dataManager);
-
-	PhoneNum phonenum;
-	phonenum._middleNum = 1234;
-	phonenum._lastNum = 5678;
-	BirthDay birthday;
-	birthday._year = 1990;
-	birthday._month = 01;
-	birthday._day = 23;
-
-	int employeeNum = 50000;
-	for (int i = 0; i < 100000; i++, employeeNum--)
-	{
-		if (employeeNum < 0)
-			employeeNum = 99999999;
-		Employee inputEmployee(employeeNum, Name("GILDONG", "HONG"), CareerLevel::CareerLevel_2, phonenum, birthday, Certi::Certi_PRO);
-		InputParameter inputParameter = { CommandType::Command_None, Option1::Option1_None, Option2::Option2_None, Column::Column_None, Column::Column_None, inputEmployee };
-		add.Command(inputParameter);
-	}
-
-	ASSERT_TRUE(dataManager.getSize() == 100000);
-
-	employeeNum = 99950001;
-	vector<Employee> employees = dataManager.getData();
-	for (auto employee : employees)
-	{
-		Employee inputEmployee(employeeNum++, Name("GILDONG", "HONG"), CareerLevel::CareerLevel_2, phonenum, birthday, Certi::Certi_PRO);
-		Employee* result = &employee;
-		EXPECT_EQ(result->GetEmployeeNum(), inputEmployee.GetEmployeeNum());
-		EXPECT_EQ(result->GetName()._firstName, inputEmployee.GetName()._firstName);
-		EXPECT_EQ(result->GetName()._lastName, inputEmployee.GetName()._lastName);
-		EXPECT_EQ(result->GetCl(), inputEmployee.GetCl());
-		EXPECT_EQ(result->GetPhoneNum()._middleNum, inputEmployee.GetPhoneNum()._middleNum);
-		EXPECT_EQ(result->GetPhoneNum()._lastNum, inputEmployee.GetPhoneNum()._lastNum);
-		EXPECT_EQ(result->GetBirthDay()._year, inputEmployee.GetBirthDay()._year);
-		EXPECT_EQ(result->GetBirthDay()._month, inputEmployee.GetBirthDay()._month);
-		EXPECT_EQ(result->GetBirthDay()._day, inputEmployee.GetBirthDay()._day);
-		EXPECT_EQ(result->GetCerti(), inputEmployee.GetCerti());
-
-		if (employeeNum >= 100000000)
-			employeeNum = 0;
-	}
-}
-
 TEST(TEST_ADD_MODULE, ADD_INPUT_EXCEPTION_TEST) {
 	DataManager dataManager = DataManager();
 	Add add = Add(dataManager);
